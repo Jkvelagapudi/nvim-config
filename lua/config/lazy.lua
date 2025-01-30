9550dd6661f6d0b24f0ac5233f11fdc4fd79eadc
jkvelagapudi <jkvelagapudi@gmail.com>
2025-01-29 22:02:19 -0600

    stuff

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,17 +15,16 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    { import = "plugins" }, -- Import all files inside the plugins folder
+    { import = "plugins" },
   },
   install = { colorscheme = { "tokyonight" } },
   checker = { enabled = true },
 })
+
