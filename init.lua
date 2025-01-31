07179bfe9a6db002ed9dff9cabe4192ce74b182f
jkvelagapudi <jkvelagapudi@gmail.com>
2025-01-30 21:47:49 -0600

    stuff 2

--- init.lua
+++ init.lua
@@ -1,1 +1,7 @@
 require("config.lazy")
+
+vim.opt.relativenumber = true
+
+vim.cmd [[ hi Normal guibg=NONE ctermbg=NONE ]]
+vim.cmd [[ hi NormalNC guibg=NONE ctermbg=NONE ]]
+vim.cmd [[ hi EndOfBuffer guibg=NONE ctermbg=NONE ]]
